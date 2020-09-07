111ec97dc7723fe9c9cbd5982ce16ba9901b91d1
Boxiang <gbx337360509@gmail.com>
2020-09-07 00:31:09 -0500

    start with swiftui

---
+++ ACTravelGuideTests/ACTravelGuideTests.swift
@@ -0,0 +1,34 @@
+//
+//  ACTravelGuideTests.swift
+//  ACTravelGuideTests
+//
+//  Created by Boxiang Guo on 9/7/20.
+//  Copyright © 2020 Boxiang Guo. All rights reserved.
+//
+
+import XCTest
+@testable import ACTravelGuide
+
+class ACTravelGuideTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
